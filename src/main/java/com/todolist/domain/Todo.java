68fa9478599d21d17bccb7be943ab82730acd5ca
suhyeoonn <suhyeon1740@gmail.com>
2024-07-23 11:58:32 +0900

    🎨 Todo 게터, 세터 추가

--- src/main/java/com/todolist/domain/Todo.java
+++ src/main/java/com/todolist/domain/Todo.java
@@ -4,4 +4,17 @@ public class Todo {
     private Long id;
     private String content;
     private Category category;
+
+    public Todo(String content, Category category) {
+        this.content = content;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
